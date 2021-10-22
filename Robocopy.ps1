ffcee550892a7efe488fccae913c1ee607b2134e
Chris Hailes <53359287+Chris-Hailes@users.noreply.github.com>
2021-10-22 17:46:09 +1100

    Update Robocopy.ps1

--- Robocopy.ps1
+++ Robocopy.ps1
@@ -5,7 +5,7 @@
  /MIR specifies that Robocopy should mirror the source directory and the destination directory. 
  /Z ensures Robocopy can resume the transfer of a large file in mid-file instead of restarting.
  /XA:H makes Robocopy ignore hidden files, usually these will be system files that we're not interested in.
- /R:5 rety failed read/writes
+ /R:5 rety failed read/writes 5 times before moving on
  /W:5 reduces the wait time between failures to 5 seconds instead of the 30 second default.
  /tee Write output to console and logfile
  /log Log output to logfile and overwrite on executions
