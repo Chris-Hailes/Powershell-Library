f62f86ab9ac21a715b01268afcf73347fc132233
Chris Hailes <53359287+Chris-Hailes@users.noreply.github.com>
2021-10-26 12:42:12 +1100

    Update Robocopy.ps1

--- Robocopy.ps1
+++ Robocopy.ps1
@@ -10,13 +10,44 @@
  /tee Write output to console and logfile
  /log Log output to logfile and overwrite on executions
 
+Once the robocopy completes the script will send an email with summary of log and include errors into the body
+
 .NOTES
  AUTHOR: Chris Hailes (cubesys)
 #>
 
 
+$EmailFrom = ''
+$EmailTo = 'chris.hailes'
+$SMTPServer = 'smtp.office365.com'
+$ServerPort = 587
+$encrypted = Get-Content c:\scripts\encrypted_password.txt | ConvertTo-SecureString
+$SMTPCredentials = New-Object System.Management.Automation.PsCredential("", $encrypted) 
+$EmailBody = "<br />"
+
 $sourcedir = 'D:\SourceDir'
 $destinationdir = 'D:\DestDir'
-$logfile = 'C:\Users\admin\desktop\robolog.txt'
+$logfile = 'C:\scripts\robolog.txt'
+
+$Start = Get-Date
 
 robocopy $sourcedir $destinationdir /MIR /Z /XA:H /R:5 /W:5 /tee /log:$logfile
+
+$Stop = Get-Date
+$TimeTaken = ($Stop - $Start).TotalMinutes
+
+#Function Send Email
+Function Send-Notification($EmailSubject,$EmailBody)
+{
+    Send-MailMessage -From $EmailFrom -To $EmailTo -Subject $EmailSubject -Body $EmailBody -BodyAsHtml -SmtpServer $SMTPServer -Credential $SMTPCredentials -Port $ServerPort -UseSsl
+}
+
+$logsummary = (Get-Content $logfile | Select -Last 14) -join "<br />"
+$logerrors = (Get-Content $logfile | Select-String -Pattern "ERROR" -SimpleMatch) -join "<br />"
+$EmailBody += "Summary <br /> $($logsummary) <br />"
+$EmailBody += "Time Taken: $($TimeTaken) Minutes <br />"
+$EmailBody += "Error Paths <br /> $($logerrors) <br /> <br />"
+
+$EmailSubject = "Robocopy Report"
+
+Send-Notification $EmailSubject $EmailBody
