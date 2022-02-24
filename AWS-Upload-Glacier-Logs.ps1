381f4d88e52e3f5ff840282079a7f879b55b7b83
Chris Hailes <53359287+Chris-Hailes@users.noreply.github.com>
2022-02-24 18:30:29 +1100

    Update AWS-Upload-Glacier-Logs.ps1
    added an additional foreach to upload the files individually and have a key as the folder path

--- AWS-Upload-Glacier-Logs.ps1
+++ AWS-Upload-Glacier-Logs.ps1
@@ -21,19 +21,25 @@ Import-Module AWS.Tools.EC2
 Import-Module AWS.Tools.S3
 
 #AWS Account Credential
-Set-AWSCredential -ProfileName npsgeneral
+Set-AWSCredential -ProfileName aws-profile
 
 #Find Log Folders
 $logpath = "C:\Temp"
 $logfolders = Get-ChildItem -Path $logpath -Recurse | Where-Object {$_.PSIsContainer -eq $true -and $_.Name -match "Logs"}
 
 Foreach ($folder in $logfolders){
-#Go to Directory
-CD $folder.FullName  
-#What Items in this path are older than 12 months, write them to Glacier
-Get-ChildItem -Path $folder.FullName | Where-Object { $_.CreationTime -lt ((get-date).AddMonths(-12)) } | Write-S3Object -BucketName nps-logarchive -StorageClass GLACIER -KeyPrefix $folder.FullName.TrimStart($logpath)
-#Leave Directory
-CD \
-#Delete the file from system
-Get-ChildItem -Path $folder.FullName | Where-Object { $_.CreationTime -lt ((get-date).AddMonths(-12)) } | Remove-Item
+	#Go to Directory
+	CD $folder.FullName  
+	#What Items in this path are older than 12 months
+	$items = Get-ChildItem -Path $folder.FullName | Where-Object { $_.CreationTime -lt ((get-date).AddMonths(-12)) } 
+	
+		#We run a foreach to upload each item individually
+		Foreach ($item in $items){
+		#This upload the item to a BuckName with a "GLACIER" Storage class (for archiving purposes), its file path will be /Folder/FileName in S3
+		Write-S3Object -BucketName ArchiveBucket -StorageClass GLACIER -File $item.Name -Key $item.FullName.TrimStart($logpath)
+		}
+	#Leave Directory
+	CD \
+	#Delete the file from system
+	Get-ChildItem -Path $folder.FullName | Where-Object { $_.CreationTime -lt ((get-date).AddMonths(-12)) } | Remove-Item
 }
