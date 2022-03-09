3948be87469b3e9b23bbe33d4e3851a4a8ba05ef
Chris Hailes <53359287+Chris-Hailes@users.noreply.github.com>
2022-03-09 17:39:55 +1100

    Azure Firewall Scripts
    Scripts to deallocate/stop and allocate/start azure firewall

---
+++ Azure-Deallocate-AzFirewall.ps1
@@ -0,0 +1,35 @@
+﻿<# 
+
+.NOTES 
++---------------------------------------------------------------------------------------------+ 
+| ORIGIN STORY                                                                                | 
++---------------------------------------------------------------------------------------------+ 
+|   DATE        : 09/03/2022                                                                  |
+|   AUTHOR      : Chris Hailes (cubesys)                                                      | 
+|   VERSION     : 1.0                                                                         | 
++---------------------------------------------------------------------------------------------+ 
+
+.SYNOPSIS 
+ This script will deallocate an Azure Firewall, this is useful when first deploying azure firewalls
+ Deallocating the firewall allows it to be deployed and configured without incurring costs.
+
+.EXAMPLE
+Azure-Deallocate-AzFirewall -Subscription "Subscription Name" -FirewallName "AzureFW" -ResourceGroup "firewall-RG"
+
+#>
+
+#Parameters
+Param (
+    [string] $Subscription = "Hub Subscription",
+    [string] $FirewallName = "AzureFW",
+    [string] $ResourceGroup = "firewall-rg"
+)
+#Set Az Context to Subscription
+Set-AzContext -Subscription $subscription
+
+#Set the Azure Firewall Variable
+$azfw = Get-AzFirewall -Name $firewallname -ResourceGroupName $resourcegroup
+#Add the Deallocate
+$azfw.Deallocate()
+#Apply the Deallocation
+Set-AzFirewall -AzureFirewall $azfw
---
+++ Azure-Start-AzFirewall.ps1
@@ -0,0 +1,39 @@
+﻿<# 
+
+.NOTES 
++---------------------------------------------------------------------------------------------+ 
+| ORIGIN STORY                                                                                | 
++---------------------------------------------------------------------------------------------+ 
+|   DATE        : 09/03/2022                                                                  |
+|   AUTHOR      : Chris Hailes (cubesys)                                                      | 
+|   VERSION     : 1.0                                                                         | 
++---------------------------------------------------------------------------------------------+ 
+
+.SYNOPSIS 
+ This script will allocate the required resources and start an Azure Firewall.
+
+.EXAMPLE
+Azure-Start-AzFirewall -Subscription "Subscription Name" -FirewallName "AzureFW" -VNetName "firewall-vnet" -PubIPName "firewall-pip" -ResourceGroup "firewall-RG"
+
+#>
+
+#Parameters
+Param (
+[string] $Subscription = "Subscription Name",
+[string] $FirewallName = "AzureFW",
+[string] $ResourceGroup = "firewall-rg",
+[string] $VnetName = "firewall-vnet",
+[string] $PubIPName = "firewall-pip"
+)
+
+#Set Az Context to Subscription
+Set-AzContext -Subscription $subscription
+
+#Set the Azure Firewall Variable
+$azfw = Get-AzFirewall -Name $firewallname -ResourceGroupName $resourcegroup
+$vnet = Get-AzVirtualNetwork -ResourceGroupName $resourcegroup -Name $vnetname
+$publicip = Get-AzPublicIpAddress -ResourceGroupName $resourcegroup -Name $pubipname
+#Allocate Configuration to Azure Firewall
+$azfw.Allocate($vnet, $publicip)
+#Start Firewall
+Set-AzFirewall -AzureFirewall $azfw
